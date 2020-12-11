f744fb8c7a57eed21f86c7521d5e92cdd6db9337
shubambhasin <shubam.developer@gmail.com>
2020-12-11 12:54:32 +0530

    added textarea for input and output

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+var textArea= document.querySelector("#text-area")
+
+var outputDiv = document.querySelector("#output");
+
+
+
+
+
+btnTranslate.addEventListener("click", function eventListenerHandler() {
+
+    outputDiv.innerHTML = "wrhljkfdn.cm   " + textArea.value;
+    
+})
+
+
+
