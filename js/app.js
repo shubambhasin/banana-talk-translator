f67facf254af3cabb371af781d487e3343a4416e
shubambhasin <shubam.developer@gmail.com>
2020-12-11 15:59:19 +0530

    feat:  added translate features

--- js/app.js
+++ js/app.js
@@ -3,15 +3,40 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textArea= document.querySelector("#text-area")
 
 var outputDiv = document.querySelector("#output");
+ 
+//  var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var url ="https://api.funtranslations.com/translate/minion.json"
 
 
 
+function getConstructURl(text)
+{
+    return url + "?" + "text="+ text;
 
+}
 
-btnTranslate.addEventListener("click", function eventListenerHandler() {
+function errorHandler(error)
+{
 
-    outputDiv.innerHTML = "wrhljkfdn.cm   " + textArea.value;
-    
+    console.log("error", error);
+
+}
+
+btnTranslate.addEventListener("click", function clickHandler() {
+
+    var inputText = textArea.value;
+
+    fetch(getConstructURl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        console.log(json.contents.translated)
+        var translatedText = json.contents.translated
+        outputDiv.innerHTML = translatedText
+    })
+    .catch(errorHandler)
+
+   
 })
 
 
